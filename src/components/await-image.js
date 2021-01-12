35381d38e345a1e7d5bfd9891d2f34731f0ed27b
AydanG <aydang@seas.upenn.edu>
2021-01-12 18:07:33 -0500

    fixed bug with await-images where partially loaded image appeared next to spinner

--- src/components/await-image.js
+++ src/components/await-image.js
@@ -2,9 +2,13 @@ import { Fragment, useState } from 'react';
 
 export default function AwaitImage(props) {
   const [loading, setLoading] = useState(true);
+  let {className, ...vettedProps} = props;
+  if (loading) {
+    className += ' d-none';
+  }
   return (
     <div>
-      <img {...props} alt="..." onLoad={() => setLoading(false)} />
+      <img {...vettedProps} className={className} alt="..." onLoad={() => setLoading(false)} />
       {loading ? (
           <div className="spinner-border text-primary" role="status">
             <span className="sr-only">Loading...</span>
